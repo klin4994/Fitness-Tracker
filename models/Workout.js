77e0309d1b548ba7b3045d8d243a64137d25fd83
klin4994 <kevlin129@hotmail.com>
2021-04-04 14:58:45 +1000

    Now able to populate database  with mongoose

---
+++ models/Workout.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+  },
+  name: {
+    type: String,
+    trim: true,
+  },
+  duration: {
+    type: Number
+  },
+  weight: {
+    type: Number
+  },
+  reps: {
+    type: Number
+  },
+  sets: {
+    type: Number
+  }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
+
